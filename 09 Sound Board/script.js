8eb5c85a9a97c56210c79bc47e5d121c93a0a1bd
Leothurm <leonardo.thurm@gmail.com>
2023-02-13 15:13:56 -0300

    Project 09 finished

--- 09 Sound Board/script.js
+++ 09 Sound Board/script.js
@@ -8,6 +8,22 @@ sounds.forEach(sound => {
 
     btn.innerText = sound
 
+    btn.addEventListener('click', () =>{
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+
+
     document.getElementById('buttons').appendChild(btn)
 
 })
+
+function stopSongs(){
+    sounds.forEach(sound =>{
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.CurrentTime = 0;
+    })
+}

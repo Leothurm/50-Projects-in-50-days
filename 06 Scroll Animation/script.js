87b80fdaa032921bc75e8314c80bcfcb04f6fe56
Leothurm <leonardo.thurm@gmail.com>
2023-02-13 14:36:11 -0300

    update JS

--- 06 Scroll Animation/script.js
+++ 06 Scroll Animation/script.js
@@ -5,15 +5,17 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes(){
-  let triggerBottom =   window.innerHeight/5*3
+  let triggerBottom =  window.innerHeight/5*3
+  console.log(triggerBottom)
 
-  boxes.forEach(box => {
-    const boxTop = box.getBoundingClientRect().top
+  boxes.forEach(b => {
+    const boxTop = b.getBoundingClientRect().top
 
-    if(boxTop< triggerBottom){
-        box.classList.add('show')
+    if(boxTop < triggerBottom){
+        b.classList.add('show')
+        
     } else{
-        box.classList.remove('show')
+        b.classList.remove('show')
     }
 })
 }

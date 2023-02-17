36ba924dadcf0a2928bf39fa8b27909fc9ebd9b4
Leothurm <leonardo.thurm@gmail.com>
2023-02-16 21:43:22 -0300

    Refatoring API request

--- 10 Dad Jokes/script.js
+++ 10 Dad Jokes/script.js
@@ -1,9 +1,13 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
+
+jokeBtn.addEventListener('click', generateJoke)
+
+
 generateJoke()
 
-function generateJoke(){
+async function generateJoke(){
 
     let config = {
         headers:{
@@ -11,9 +15,27 @@ function generateJoke(){
         },
     }
 
-    fetch('https://icanhazdadjoke.com/', config)
-    .then((res) => res.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    })
+   const res = await fetch('https://icanhazdadjoke.com/', config)
+   
+
+    const data = await res.json()
+
+
+    jokeEl.innerHTML = data.joke
 }
+
+
+// function generateJoke(){
+
+//     let config = {
+//         headers:{
+//             Accept:'application/json'
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }

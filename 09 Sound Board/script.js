bcd9812a87c6f9f321a9e64973259d6304b9d3b5
Leothurm <leonardo.thurm@gmail.com>
2023-02-18 00:01:07 -0300

    fix problems

--- 09 Sound Board/script.js
+++ 09 Sound Board/script.js
@@ -3,7 +3,6 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
-    console.log(btn)
 
 
     btn.innerText = sound

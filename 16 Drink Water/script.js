e1ae70da3c5f1e513c47bc1f1bf7500defdb3255
Leothurm <leonardo.thurm@gmail.com>
2023-03-04 23:57:51 -0300

    JavaScript finished

--- 16 Drink Water/script.js
+++ 16 Drink Water/script.js
@@ -3,6 +3,9 @@ const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
+
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlighCups(idx));
 });
@@ -22,4 +25,33 @@ function highlighCups(idx) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup()
+
+}
+
+
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+
+    const totalCups = smallCups.length
+
+    if(fullCups === 0){
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else{
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups/totalCups*330}px  `
+        percentage.innerText = `${fullCups/totalCups *100}%`
+    }
+
+    if(fullCups === totalCups){
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+        percentage.innerText = `You got it!`
+
+    } else{
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2-(250*fullCups/1000)}L`
+    }
 }

15c0ec3486c6aeabff9feb9a9876b1923ca0b8a4
Leothurm <leonardo.thurm@gmail.com>
2023-03-09 10:01:49 -0300

    Getting dynamic page with JS & API

--- 17 Movie App/script.js
+++ 17 Movie App/script.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -12,9 +13,54 @@ getMovies(API_URL)
 async function getMovies(url){
     const res = await fetch(url)
     const data = await res.json()
+ 
+    /* console.log para ver oq está retornando da api */
+    //console.log(data.results)
 
-    console.log(data.results)
+    showMovies(data.results)
 }
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie
+        
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)   
+    })
+}
+
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green'
+    } else if(vote >=5){
+        return 'orange'
+    } else{
+        return 'red'
+    }
+}
+
+
+
+
+
 
 form.addEventListener('submit', (e) => {
    //o preventDefault serve para impedir que o submit atualize a página

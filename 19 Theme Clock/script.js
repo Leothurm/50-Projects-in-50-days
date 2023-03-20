ea95a6f90c99014ef1ed8136831a8b6921cd3276
Leothurm <leonardo.thurm@gmail.com>
2023-03-20 20:46:55 -0300

    clock animation with JS

--- 19 Theme Clock/script.js
+++ 19 Theme Clock/script.js
@@ -11,6 +11,8 @@ const days = ["Sunday","Monday","Tuesday","Wednesday",
 const months = ["Jan","Feb","Mar","Apr",
 "May","Jun","Jul", "Aug","Sep","Oct","Nov","Dec"];
 
+
+//Dark Mode
 toggle.addEventListener('click',(e) =>{
     const html = document.querySelector('html')
     if(html.classList.contains('dark')){
@@ -21,3 +23,29 @@ toggle.addEventListener('click',(e) =>{
         e.target.innerHTML = 'Light mode'
     }
 })
+
+//
+function setTime(){
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const hours = time.getHours()
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0,11,0,360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0,59,0,360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0,11,0,360)}deg)`
+}
+
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+setTime()
+
+setInterval(setTime, 1000)

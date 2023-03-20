0ea6fc5e4f68ea1f3a990ad8306f683c58da4f2f
Leothurm <leonardo.thurm@gmail.com>
2023-03-20 20:49:54 -0300

    update

--- 19 Theme Clock/script.js
+++ 19 Theme Clock/script.js
@@ -13,39 +13,41 @@ const months = ["Jan","Feb","Mar","Apr",
 
 
 //Dark Mode
-toggle.addEventListener('click',(e) =>{
-    const html = document.querySelector('html')
-    if(html.classList.contains('dark')){
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
-    } else{
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light mode'
+    toggle.addEventListener('click',(e) =>{
+        const html = document.querySelector('html')
+        if(html.classList.contains('dark')){
+            html.classList.remove('dark')
+            e.target.innerHTML = 'Dark mode'
+        } else{
+            html.classList.add('dark')
+            e.target.innerHTML = 'Light mode'
+        }
+    })
+
+// Configuração do relógio
+    function setTime(){
+        const time = new Date();
+        const month = time.getMonth()
+        const day = time.getDay()
+        const hours = time.getHours()
+        const hoursForClock = hours % 12
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+
+        hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0,11,0,360)}deg)`
+        minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0,59,0,360)}deg)`
+        secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0,11,0,360)}deg)`
     }
-})
+
+
+    // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+
+    setTime()
+
+    setInterval(setTime, 1000)
 
 //
-function setTime(){
-    const time = new Date();
-    const month = time.getMonth()
-    const day = time.getDay()
-    const hours = time.getHours()
-    const hoursForClock = hours % 12
-    const minutes = time.getMinutes()
-    const seconds = time.getSeconds()
-
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0,11,0,360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0,59,0,360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0,11,0,360)}deg)`
-}
-
-
-// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-
-setTime()
-
-setInterval(setTime, 1000)

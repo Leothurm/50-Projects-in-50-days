c4c41f9b5bfb9ee74bbe79160de29bb8ad225100
Leothurm <leonardo.thurm@gmail.com>
2023-03-28 20:57:14 -0300

    Drawing lines with JS

--- 22 Drawing App/script.js
+++ 22 Drawing App/script.js
@@ -3,7 +3,8 @@ const ctx = canvas.getContext('2d')
 
 let size = 20
 let color = 'black'
-
+let x
+let y
 
 function drawCircle(x,y){
     ctx.beginPath()
@@ -13,5 +14,14 @@ function drawCircle(x,y){
 
 }
 
+function drawLine(x1,y1,x2,y2){
+    ctx.beginPath()
+    ctx.moveTo(x1,y1)
+    ctx.lineTo(x2,y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size*0.2
+    ctx.stroke()
+}
 
 drawCircle(100,200)
+drawLine(300,300,600,600)

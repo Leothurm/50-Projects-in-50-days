758eacb467b776b3e430b53ca84694ff2d168e0f
Leothurm <leonardo.thurm@gmail.com>
2023-03-29 23:07:15 -0300

    Drawing funcionality

--- 22 Drawing App/script.js
+++ 22 Drawing App/script.js
@@ -2,9 +2,42 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 let size = 20
+let isPressed = false
 let color = 'black'
 let x
 let y
+
+
+canvas.addEventListener('mousedown', (e) =>{
+    isPressed = true
+    x = e.offsetX
+    y = e.offsetY
+})
+
+
+canvas.addEventListener('mouseup', (e) =>{
+    isPressed = false
+    x = undefined
+    y = undefined
+})
+
+
+canvas.addEventListener('mousemove', (e) =>{
+    if(isPressed){
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2,y2)
+        /*console.log(x2,y2)*/
+        drawLine(x,y,x2,y2)
+
+        x=x2
+        y=y2
+    }
+
+
+})
+
 
 function drawCircle(x,y){
     ctx.beginPath()
@@ -19,7 +52,7 @@ function drawLine(x1,y1,x2,y2){
     ctx.moveTo(x1,y1)
     ctx.lineTo(x2,y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size*0.2
+    ctx.lineWidth = size*2
     ctx.stroke()
 }
 

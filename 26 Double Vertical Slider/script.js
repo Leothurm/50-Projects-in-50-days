ef9f9a364117a51557bbc4854b9d67f561cecc87
Leothurm <leonardo.thurm@gmail.com>
2023-04-10 21:00:25 -0300

    JS parte 1

---
+++ 26 Double Vertical Slider/script.js
@@ -0,0 +1,24 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1)*100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    // console.log(sliderHeight)
+    if(direction === 'up'){
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1){
+            activeSlideIndex = 0
+        }
+    }
+}

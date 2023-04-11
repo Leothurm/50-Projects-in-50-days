3ea9e3c9ce90c80fd3159e436c979ba0484df267
Leothurm <leonardo.thurm@gmail.com>
2023-04-10 21:08:48 -0300

    JS finished

--- 26 Double Vertical Slider/script.js
+++ 26 Double Vertical Slider/script.js
@@ -20,5 +20,13 @@ const changeSlide = (direction) => {
         if(activeSlideIndex > slidesLength - 1){
             activeSlideIndex = 0
         }
+    } else if(direction === 'down'){
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesLength - 1
+        }
     }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }

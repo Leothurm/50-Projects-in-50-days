bd23d4647d37c7469468231c3332c75f34749d38
Leothurm <leonardo.thurm@gmail.com>
2023-04-16 20:28:04 -0300

    adding profile card

--- 28 GitHub Profiles/script.js
+++ 28 GitHub Profiles/script.js
@@ -1,5 +1,6 @@
 const APIURL ='https://api.github.com/users/'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -16,13 +17,51 @@ async function getUser(username){
     try{
         const {data} = await axios(APIURL + username)
 
-        console.log(data)
+        createUserCard(data)
     } catch (err){
-        console.log(err)
+
+        if(err.response.status == 404){
+            createErrorCard('No profile with this username')
+        }
     }
 
 }
 
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+
+        <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+    </div>
+</div>
+`
+
+main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg){
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+
+}
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
